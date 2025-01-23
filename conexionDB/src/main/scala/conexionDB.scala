fd47b644bbdf73de40c3472338e236ccb6522cd0
AndyJunioRR2 <andyvein20@gmail.com>
2025-01-23 11:49:07 -0500

    conexion

--- conexionDB/src/main/scala/conexionDB.scala
+++ conexionDB/src/main/scala/conexionDB.scala
@@ -1,1 +1,39 @@
-object conexionDB
+import java.sql.{Connection, DriverManager, ResultSet}
+
+object ConexionMySQL {
+
+  val url = "jdbc:mysql://localhost:3306/prueba"
+  val username = "Andy"
+  val password = "andyjunior"
+
+  def main(args: Array[String]): Unit = {
+    var connection: Connection = null
+
+    try {
+
+      connection = DriverManager.getConnection(url, username, password)
+      println("Conexión exitosa a la base de datos.")
+
+
+      val statement = connection.createStatement()
+      val resultSet: ResultSet = statement.executeQuery("SELECT * FROM usuarios")
+
+
+      while (resultSet.next()) {
+        val id = resultSet.getInt("id")
+        val nombre = resultSet.getString("nombre")
+        val correo = resultSet.getString("correo")
+        println(s"ID: $id, Nombre: $nombre, Correo: $correo")
+      }
+
+    } catch {
+      case e: Exception =>
+        e.printStackTrace()
+    } finally {
+      if (connection != null) {
+        connection.close()
+        println("Conexión cerrada.")
+      }
+    }
+  }
+}
